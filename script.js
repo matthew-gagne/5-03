5067ff8e2d7c9f6b05047d4bb592df99eddcfd01
matthewgagne1 <>
2021-06-02 13:10:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('alert').addEventListener('click', reportResults);
+/*the computer reads what is in the text box and dispalys that as an alert when you press the button
+*/
+
+function reportResults () {
+  alert(document.getElementById('text').value)
+}
